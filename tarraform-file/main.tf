238f5bb4751ff1c006515230af2f7a8536d93394
snehal parange <104732807+Parangesnehal@users.noreply.github.com>
2024-09-12 01:19:17 +0530

    Create main.tf

---
+++ tarraform-file/main.tf
@@ -0,0 +1,24 @@
+resource "aws_instance" "test-server" {
+  ami = "ami-0522ab6e1ddcc7055"
+  instance_type = "t2.micro"
+  key_name = "cp"
+  vpc_security_group_ids = ["sg-0c559e86ad6ca8d96"]
+  connection {
+     type = "ssh"
+     user = "ubuntu"
+     private_key = file("./cp.pem")
+     host = self.public_ip
+     }
+  provisioner "remote-exec" {
+     inline = ["echo 'wait to start the instance' "]
+  }
+  tags = {
+     Name = "test-server"
+     }
+  provisioner "local-exec" {
+     command = "echo ${aws_instance.test-server.public_ip} > inventory"
+     }
+  provisioner "local-exec" {
+     command = "ansible-playbook /var/lib/jenkins/workspace/Care-Health/terraform-files/ansibleplaybook.yml"
+     }
+  }
